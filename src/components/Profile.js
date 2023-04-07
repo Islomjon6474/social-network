e1cfecf219ba1ebceaf35b8eefaa9e05c2fac508
Islomjon6474 <islomjonrahimjonov6474@gmail.com>
2023-04-07 21:56:31 +0500

    first commit

---
+++ src/components/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+        <div>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjPo4SvPmw5ufuWuFVG4fHYlv7P-Yf7WPkBA&usqp=CAU"
+            alt=""
+          />
+        </div>
+        <div>ava + desc</div>
+        <div>
+          My post
+          <div>New posts</div>
+        </div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    );
+}
+
+export default Profile;
